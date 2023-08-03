cb9f78a53992645e12831d0a1110cfcf8a8ec6bf
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-08-03 18:38:09 +0000

    Add products app with category, product and review models

---
+++ products/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    brand = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    rating = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review {self.body} by (self.author)"
