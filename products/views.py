93117d57dd4fd3399527e41274379f9b9c153d12
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-08-08 17:46:44 +0000

    Add product detail page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Product
 
@@ -14,5 +14,16 @@ def all_products(request):
     template = "products/products.html"
     context = {
         "page_obj": page_obj
+    }
+    return render(request, template, context)
+
+
+def product_detail(request, product_id):
+    """ A view to show a detailed product page """
+
+    product = get_object_or_404(Product, pk=product_id)
+    template = "products/product_detail.html"
+    context = {
+        "product": product
     }
     return render(request, template, context)
