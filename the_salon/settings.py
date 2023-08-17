1ce7e02185177e98a49212427d0057ce3708df04
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-08-17 14:46:30 +0000

    Add cart contexts.py and delivery logic

--- the_salon/settings.py
+++ the_salon/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.media',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -167,3 +168,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 60
+STANDARD_DELIVERY = 5.50
