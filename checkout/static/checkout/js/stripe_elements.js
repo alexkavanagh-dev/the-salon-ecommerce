91938be53780dac64537e204859fe3db728f2226
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-08-30 14:21:56 +0000

    Added checkout form data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -42,27 +42,64 @@ form.addEventListener('submit', function(event) {
   document.getElementById('loading-overlay').classList.remove('d-none');
   document.getElementById('loading-spinner').classList.remove('d-none');
 
-  stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
+  let saveInfo = Boolean($('#id-save-info').attr('checked'));
+  // From using {% csrf_token %} in the form
+  let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+  let postData = {
+    'csrfmiddlewaretoken': csrfToken,
+    'client_secret': clientSecret,
+    'save_info': saveInfo,
+  };
+  let url = '/checkout/cache_checkout_data/';
+
+  $.post(url, postData).done(function () {
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
           card: card,
-      }
-  }).then(function(result) {
-      if (result.error) {
-          let errorDiv = document.getElementById('card-errors');
-          let html = `
-              <span class="icon" role="alert">
-              <i class="fas fa-times"></i>
-              </span>
-              <span>${result.error.message}</span>`;
-          errorDiv.innerHTML(html);
-          document.getElementById('loading-overlay').classList.add('d-none');
-          document.getElementById('loading-spinner').classList.add('d-none');
-          card.update({ 'disabled': false});
-          document.getElementById('submit-button').setAttribute('disabled', false);
-      } else {
-          if (result.paymentIntent.status === 'succeeded') {
-              form.submit();
+          billing_details: {
+              name: form.full_name.value.trim(),
+              phone: form.phone_number.value.trim(),
+              email: form.email.value.trim(),
+              address:{
+                  line1: form.street_address1.value.trim(),
+                  line2: form.street_address2.value.trim(),
+                  city: form.town_or_city.value.trim(),
+                  state: form.county.value.trim(),
+              }
           }
-      }
-  });
+      },
+      shipping: {
+          name: form.full_name.value.trim(),
+          phone: form.phone_number.value.trim(),
+          address: {
+              line1: form.street_address1.value.trim(),
+              line2: form.street_address2.value.trim(),
+              city: form.town_or_city.value.trim(),
+              postal_code: form.postcode.value.trim(),
+              state: form.county.value.trim(),
+          }
+      },
+    }).then(function(result) {
+        if (result.error) {
+            let errorDiv = document.getElementById('card-errors');
+            let html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            errorDiv.innerHTML(html);
+            document.getElementById('loading-overlay').classList.add('d-none');
+            document.getElementById('loading-spinner').classList.add('d-none');
+            card.update({ 'disabled': false});
+            document.getElementById('submit-button').setAttribute('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+      });
+  }).fail(function () {
+    // just reload the page, the error will be in django messages
+    location.reload();
+  })
 });
