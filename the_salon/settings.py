0256ec08d00388ee68c86dad1dee9ba088e1f0cb
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-09-03 19:51:01 +0000

    Add confirmation emails for orders

--- the_salon/settings.py
+++ the_salon/settings.py
@@ -186,6 +186,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
+DEFAULT_FROM_EMAIL = 'thesalon@example.com'
 
 # Countries
 
