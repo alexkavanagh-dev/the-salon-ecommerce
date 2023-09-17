e0f7673458624acbd7c132000fbf439fdcb1e77d
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-09-17 13:44:27 +0000

    Add cruelty free section to homepage

--- home/views.py
+++ home/views.py
@@ -12,8 +12,12 @@ def index(request):
     feat_products = products.filter(brand__icontains='Olaplex')
     feat_products = feat_products[:4]
 
+    cf_products = products.filter(cruelty_free=True)
+    cf_products = cf_products[:4]
+
     context = {
         'feat_products': feat_products,
+        'cf_products': cf_products,
     }
 
     return render(request, 'home/index.html', context)
